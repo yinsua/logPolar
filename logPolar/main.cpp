1922983cd83e6435f89d19e1f6a6662ac8b404a0
yinsua <ysh.001@qq.com>
2016-03-08 11:05:52 +0800

    Create

---
+++ logPolar/main.cpp
@@ -0,0 +1,45 @@
+
+#include <iostream>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+using namespace std;
+
+int main(int argc, char ** argv){
+	IplImage* src;
+	double M;
+	src = cvLoadImage("D:\Tulips.jpg",1);
+	if (!src) {
+		cout << " open picture failed";
+		cvWaitKey();
+		return 0;
+	}
+
+	M = atof("0.8");
+	IplImage* dst = cvCreateImage( cvGetSize(src), 8, 3 );
+	IplImage* src2 = cvCreateImage( cvGetSize(src), 8, 3 );
+	cvLogPolar(
+		src,
+		dst,
+		cvPoint2D32f(src->width/4,src->height/2),
+		M,
+		CV_INTER_LINEAR+CV_WARP_FILL_OUTLIERS
+		);
+	cvLogPolar(
+		dst,
+		src2,
+		cvPoint2D32f(src->width/4,src->height/2),
+		M,
+		CV_INTER_LINEAR | CV_WARP_INVERSE_MAP
+		);
+	cvNamedWindow( "src",1 );
+	cvShowImage( "src",src );
+	cvNamedWindow( "log-polar",1 );
+	cvShowImage( "log-polar",dst );
+	cvNamedWindow( "inverse log-polar",1 );
+	cvShowImage( "inverse log-polar",src2 );
+	cvWaitKey();
+
+	return 0;
+}
+
